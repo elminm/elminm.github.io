d7aa5131f26eef45192b12dd44f6c01cb29fb909
Elmin-web <elmin555@bk.ru>
2023-03-29 20:06:19 +0400

    pages

---
+++ precache-manifest.b2329927a3ddcfb0ec18600bd7274381.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "544e53637a32a61199f3640b48c1a3d0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e0d50e28f038ab74b215",
+    "url": "/static/css/main.fdf04810.chunk.css"
+  },
+  {
+    "revision": "0950b2f147eea1d7ff9e",
+    "url": "/static/js/2.2804170f.chunk.js"
+  },
+  {
+    "revision": "ab8f792973d32f9b93c607ee81282492",
+    "url": "/static/js/2.2804170f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e0d50e28f038ab74b215",
+    "url": "/static/js/main.e5bf6831.chunk.js"
+  },
+  {
+    "revision": "97d614f48baeada74120",
+    "url": "/static/js/runtime-main.47eaace9.js"
+  }
+]);
